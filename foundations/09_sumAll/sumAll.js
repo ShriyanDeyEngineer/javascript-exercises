4f90c7f43f473ac7132094b043370a36481aa3e8
ShriyanDeyEngineer <shriyandey07@gmail.com>
2026-07-31 23:33:16 -0500

    Complete exercise, 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((typeof(num1) != "number" || typeof(num2) != "number") ||  
+    (num1 < 1 || num2 < 1) ||
+    (num1 % 1 != 0 || num2 % 1 != 0))
+    {return "ERROR";}
 
+    let lowBound, highBound, total = 0;
+
+    if(num1 < num2){lowBound = num1; highBound = num2;}
+    else{lowBound = num2; highBound = num1;}
+
+    for(let i = lowBound; i <= highBound; i++)
+    {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
