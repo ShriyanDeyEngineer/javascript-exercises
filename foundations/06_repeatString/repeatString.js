837df91b3b165580b16e676af0670a81d996796d
ShriyanDeyEngineer <shriyandey07@gmail.com>
2026-07-31 20:24:12 -0500

    Complete exercise, 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, repeats) {
+    if(repeats < 0)
+    {
+        return "ERROR";
+    }
 
+    let repeatStr = "";
+    for(let i = 0; i < repeats; i++)
+    {
+        repeatStr += str;
+    }
+    return repeatStr;
 };
 
 // Do not edit below this line
