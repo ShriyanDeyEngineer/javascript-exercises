e789e29556c1df078660ccf17ed7f8afbf748188
ShriyanDeyEngineer <shriyandey07@gmail.com>
2026-08-03 21:18:59 -0500

    Complete exercise, 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, num) => product * num);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    let multiplier = num - 1;
+    while(multiplier >= 1)
+    {
+      num *= multiplier;
+      multiplier -= 1;
+    }
+  }
+
+  return num;
 };
 
 // Do not edit below this line
