29dcf97d84da6ff7922f03ca5547a691bf9e3e47
ShriyanDeyEngineer <shriyandey07@gmail.com>
2026-08-04 00:03:17 -0500

    Complete exercise, 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index); 
+    if(index < 0){return "OOPS";}else if(index == 0){return 0;}
 
+    let fibonacciList = [1, 1];
+
+    for(let i = 1; i < index; i++)
+    {
+        fibonacciList.push(fibonacciList[i] + fibonacciList[i - 1]);
+    }
+
+    return fibonacciList[index - 1];
 };
 
 // Do not edit below this line
